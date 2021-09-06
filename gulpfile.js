57dcb4ff7bfcb38486aea182cb504717a3f9e747
Iss <0I0akel09@gmail.com>
2021-09-06 15:21:08 +0300

    added postcss

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,39 @@
 // }
 
 // exports.default = defaultTask;
-
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
-gulp.task('styles', function(){
- return gulp.src('styles/index.css')
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-  .pipe(gulp.dest('styles'))
-});
+const { series, watch } = require('gulp');
+
+function styles(cb) {
+  var plugins = [cssImport, cssvars, nested, autoprefixer];
+  gulp.src('styles/index.css')
+      .pipe(postcss(plugins))
+      .pipe(gulp.dest('styles/rebuild'));
+  cb();
+}
+
+function reloadBrowser(cb){
+  browserSync.reload();
+  cb();
+}
+
+
+exports.default = function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+  watch('./styles/index.css', series(styles, reloadBrowser));
+  watch('index.html',series(reloadBrowser));
+}
+
+exports.styles = styles;
