b88e172b417338fe99e59c735d54deb57864dfc0
Iss <0I0akel09@gmail.com>
2021-09-08 20:38:23 +0300

    fixed server and plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,4 @@
-// function defaultTask(cb) {
-//   // place code for your default task here
-//   cb();
-// }
 
-// exports.default = defaultTask;
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
@@ -11,12 +6,14 @@ var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
 var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber');
 
 const { series, watch } = require('gulp');
 
 function styles(cb) {
   var plugins = [cssImport, cssvars, nested, autoprefixer];
   gulp.src('styles/index.css')
+      .pipe(plumber())
       .pipe(postcss(plugins))
       .pipe(gulp.dest('styles/rebuild'));
   cb();
@@ -28,7 +25,7 @@ function reloadBrowser(cb){
 }
 
 
-exports.default = function(){
+exports.default = function(cb){
     browserSync.init({
         server: {
             baseDir: "./"
@@ -37,5 +34,6 @@ exports.default = function(){
 
   watch('./styles/index.css', series(styles, reloadBrowser));
   watch('index.html',series(reloadBrowser));
+  cb();
 }
 
